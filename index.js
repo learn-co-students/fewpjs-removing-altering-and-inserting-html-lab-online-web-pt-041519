b0fdcbde8f9fe1d612e7192cb434fb22e2f0d80b
aparkening <aaron.parkening@gmail.com>
2019-10-25 11:57:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Remove main#main
+document.querySelector('main#main').remove();
+
+// Add h1#victory with name in phrase 
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "George is the champion!";
+document.body.appendChild(newHeader);
+
+// As function
+// function newHeader(name) {
+//   let h1 = document.createElement('h1');
+//   h1.id = "victory";
+//   h1.innerHTML = `${name} is the champion!`;
+//   document.body.appendChild(h1);
+// }
