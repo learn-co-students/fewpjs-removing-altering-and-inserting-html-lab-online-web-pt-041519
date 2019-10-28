4ed0bb415e1ce47e6b20ebb049e1b4ea64786b6d
Renee Nordholm <reneenordholm@gmail.com>
2019-10-28 16:16:54 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ main.remove();
 let newHeader = document.createElement('h1');
     newHeader.id = 'victory';
     newHeader.innerHTML = 'Renee is the champion';
+
+    document.body.appendChild(newHeader);
