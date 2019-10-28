14efee1389e4dce00606847cb96eec026108ef2d
CindyChenx <chen_cx0128@hotmail.com>
2019-10-28 01:32:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+
+// let element = document.querySelector('h1#victory');
+// element.innerHTML = 'newHeader';
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Cindy is the champion';
