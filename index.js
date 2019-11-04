07a5986fb1313aaca2be449302f2ad9334320374
github username <github email address>
2019-11-04 00:09:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+// no longer has DOM node 'main#main'
+let element = document.querySelector("main#main").remove();
+
+// has a 'newHeader' variable that points to node 'h1#victory'
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "PamPam is the champion";
