007c81006a100ac784642ed7b735a243bc9f62ed
github username <github email address>
2019-11-13 17:25:45 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-main = document.querySelector('main');
-document.body.removeChild(main);
+main = document.querySelector('main'); // find DOM node 'main'
+document.body.removeChild(main); // find parent 'body' and remove child 'main'
 
-let newHeader = document.createElement('h1');
-newHeader.setAttribute('id', 'victory');
-newHeader.innerHTML = 'Kelypso is the champion';
-document.body.appendChid(newHeader);
+let newHeader = document.createElement('h1'); // create newHeader var
+newHeader.setAttribute('id', 'victory'); // set newHeader id to 'victory'
+newHeader.innerHTML = 'Kelypso is the champion'; // set inner HTML
+document.body.appendChild(newHeader); // append newHeader to the body
